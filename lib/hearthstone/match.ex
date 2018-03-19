570468c4f2a48df07f10dd49cce73a7bf9da4de9
Juan <juanroderosales@gmail.com>
2018-03-19 11:10:06 -0400

    most game rules enforced, now need to clean dead minions

--- lib/hearthstone/match.ex
+++ lib/hearthstone/match.ex
@@ -29,6 +29,7 @@ defmodule Hearthstone.Match do
     {deck, hand} = draw(game["#{player}"])
     
     player_state = game["#{player}"]
+    |> Map.put(:minions, able_minions(game["#{player}"].minions))
     |> Map.put(:mana, replenish_mana(game["turn_num"]))
     |> Map.put(:deck, deck)
     |> Map.put(:hand, hand)
@@ -41,6 +42,16 @@ defmodule Hearthstone.Match do
     end 
   end
 
+  def able_minions(minions) do
+    Enum.map(minions, fn (min) ->
+        if min.can_attack do
+          min
+        else 
+          Map.put(min, :can_attack, true)
+        end
+    end)
+  end
+
   def replenish_mana(turn_num) when turn_num <= 10 do
     turn_num
   end
@@ -51,7 +62,7 @@ defmodule Hearthstone.Match do
   def draw(player_state) do 
     if length(player_state.deck) > 0 do
       [head | tail] = player_state.deck
-      {tail, player_state.hand ++ [head]}
+      {tail, player_state.hand ++ [get_card(head)]}
     else 
       {player_state.deck, player_state.hand}
     end
@@ -63,19 +74,22 @@ defmodule Hearthstone.Match do
       id: head,
       attack: card.attack,
       health: card.health,
+      cost: card.cost,
+      can_attack: false,
     }    
   end
 
   #TODO
-  def place(game, player, card_id) do 
+  def place(game, player, card_index) do 
     player_state = game["#{player}"]
 
-    card = Game.get_card(card_id)
+    #card = Game.get_card(card_id)
+    card = Enum.at(player_state.hand, card_index)
 
     if card.cost <= player_state.mana do
       updated_ps = Map.put(player_state, :mana, player_state.mana - card.cost)
-      |> Map.put(:minions, player_state.minions ++ [card.id])
-      |> Map.put(:hand, List.delete(player_state.hand, card.id)) 
+      |> Map.put(:minions, player_state.minions ++ [card])
+      |> Map.put(:hand, List.delete(player_state.hand, card)) 
       game = Map.put(game, player, updated_ps)
       {:ok, game}
     else
@@ -83,34 +97,59 @@ defmodule Hearthstone.Match do
     end
   end
 
-  #TODO: hardcoded player 2
-  def attack_hero(game, player, card_id) do
-    card = Game.get_card(card_id)
-    if player == "player1" do 
-      player_state = game["player2"] 
-      |> Map.put(:health, game["player2"].health - card.attack)
-      game = Map.put(game, "player2", player_state)
-    else
-      player_state = game["player1"]
-      |> Map.put(:health, game["player1"].health - card.attack)
-      game = Map.put(game, "player1", player_state)
-    end
+  #TODO: minions can only attack once
+  def attack_hero(game, player, card_index) do
+    #card = Game.get_card(card_id)
+    card = Enum.at(game["#{player}"].minions, card_index)
+    #if player == "player1" do 
+    #  player_state = game["player2"] 
+    #  |> Map.put(:health, game["player2"].health - card.attack)
+
+    #  card = Map.put(card, :can_attack, false)
+    #  opp
+    #  game = Map.put(game, "player2", player_state)
+    #else
+    #  player_state = game["player1"]
+    #  |> Map.put(:health, game["player1"].health - card.attack)
+    #  game = Map.put(game, "player1", player_state)
+    #end
+    opp_state = Map.put(game["#{opp_player(player)}"], :health, game["#{opp_player(player)}"].health - card.attack)
+    card = Map.put(card, :can_attack, false)
+    player_state = game["#{player}"]
+    player_state = Map.put(player_state, :minions, List.replace_at(player_state.minions, card_index, card))
+    Map.put(game, player, player_state)
+    |> Map.put(opp_player(player), opp_state)
   end
 
   # Prototype by index of card in list
   # Something else 
-  def attack_minion(game, player, card, o_card) do
+  # TODO: Check if minion can attack
+  def attack_minion(game, player, card_ind, ocard_ind) do
     p1 = game["player1"]
     p2 = game["player2"]
     if player == "player1" do
-      attack(p1, p2)
+      att = Enum.at(p1.minions, card_ind)
+      rec = Enum.at(p2.minions, ocard_ind)
+      {att, rec} = attack(att, rec)
+      p1 = Map.put(p1, :minions, List.replace_at(p1.minions, card_ind, att))
+      p2 = Map.put(p2, :minions, List.replace_at(p2.minions, ocard_ind, rec))
+      Map.put(game, "player1", p1)
+      |> Map.put("player2", p2)
     else
-      attack(p2, p1)
+      att = Enum.at(p2.minions, card_ind)
+      rec = Enum.at(p1.minions, ocard_ind)
+      {att, rec} = attack(att, rec)
+      p1 = Map.put(p1, :minions, List.replace_at(p1.minions, ocard_ind, rec))
+      p2 = Map.put(p2, :minions, List.replace_at(p2.minions, card_ind, att))
+      Map.put(game, "player1", p1)
+      |> Map.put("player2", p2)
     end
   end
 
   def attack(att, rec) do
-    
+    att_h = att.health - rec.attack
+    rec_h = rec.health - att.attack
+    {Map.put(att, :health, att_h) |> Map.put(:can_attack, false), Map.put(rec, :health, rec_h)}
   end
 
   def opp_player("player1"), do: "player2"
@@ -123,6 +162,23 @@ defmodule Hearthstone.Match do
     else 
       false
     end
+  end
+
+  # Filters view appropriately for each player
+  def game_view(game, player) do
+    curr_p = game["#{player}"]
+    opp_p = game["#{opp_player(player)}"]
+    view = %{}
+    |> Map.put(:deck, length(curr_p.deck))
+    |> Map.put(:opp_deck, length(opp_p.deck))
+    |> Map.put(:hand, curr_p.hand)
+    |> Map.put(:opp_hand, length(opp_p.hand))
+    |> Map.put(:minions, curr_p.minions)   #TODO: how to get rid of dead minions? here or before?
+    |> Map.put(:opp_minions, opp_p.minions) #TODO: only want id's or full map?
+    |> Map.put(:mana, curr_p.mana)
+    |> Map.put(:opp_mana, opp_p.mana)
+    |> Map.put(:health, curr_p.health)
+    |> Map.put(:opp_health, opp_p.health)
   end
 
 end
